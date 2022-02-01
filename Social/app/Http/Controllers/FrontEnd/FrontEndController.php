df5eaea103f35021dda1c16e25b722ca6abe41a3
menna <fullaloka38@gmail.com>
2022-02-01 23:38:43 +0200

    admin dashboard

---
+++ Social/app/Http/Controllers/FrontEnd/FrontEndController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\FrontEnd;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FrontEndController extends Controller
+{
+
+
+
+}
+
+
